3f2ff40eddefd39dd407af623992702731550908
javierromay <javiromay911@gmail.com>
2024-08-01 14:28:48 +0200

    feat: add example data and datasets module

--- src/mypkg/__init__.py
+++ src/mypkg/__init__.py
@@ -1,3 +1,8 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("mypkg")
+
+# populate package namespace (para tener count_words... en el directorio de namespaces de mypkg al hacer import mypkg)
+from mypkg.mypkg import count_words
+from mypkg.plotting import plot_words
+from mypkg.datasets import get_flatland
